a17b15fc92f201f5e107ebaad1677655091b046a
Daniel Ho <dho799@berkeley.edu>
2022-04-22 22:57:48 +0000

    installable package

---
+++ ligotools/tests/.ipynb_checkpoints/tests_readligo-checkpoint.py
@@ -0,0 +1,13 @@
+from ligotools import readligo
+
+def test_read_frame():
+    assert 1 == 1
+
+def test_read_hdf5():
+    assert read_hdf5() != null
+
+def test_loaddata():
+    assert loaddata() != null
+    
+def test_dq2segs():
+    assert dq2segs() != null
